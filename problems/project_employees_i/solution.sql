d153ba36309f54f0de3002270472d327181d76d2
Aleksandr Mikhailov <hempalex@gmail.com>
2023-05-25 09:51:12 +0000

    Sync LeetCode submission - Project Employees I (mysql)

---
+++ problems/project_employees_i/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT p.project_id, ROUND(AVG(e.experience_years), 2) as average_years 
+FROM
+    Project p INNER JOIN Employee e ON (e.employee_id = p.employee_id)
+
+GROUP BY p.project_id
