046fd1e2da3402dfac1c039ee4d0be741c088ff0
Aleksandr Mikhailov <hempalex@gmail.com>
2023-06-02 21:50:56 +0000

    Sync LeetCode submission - Partition Labels (python3)

---
+++ problems/partition_labels/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        last = {}
+        for i, c in enumerate(s):
+            last[c] = i
+
+        res = []
+        start = end = 0 
+
+        for i, c in enumerate(s):
+            end = max(end, last.get(c, 0))
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
+
+        return res
+
+
