78485d5ddac6e8e64352ef85d525b2e7c2c6236d
Aleksandr Mikhailov <hempalex@gmail.com>
2023-06-15 11:36:37 +0000

    Sync LeetCode submission - Minimum Absolute Difference in BST (python3)

---
+++ problems/minimum_absolute_difference_in_bst/solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+
+        prev = None
+        min_diff = float('inf')
+
+        def inorder(root: Optional[TreeNode]):
+            nonlocal prev, min_diff
+
+            if not root:
+                return
+
+            inorder(root.left)
+            if prev:
+                min_diff = min(min_diff, root.val - prev.val)
+            prev = root
+            inorder(root.right)
+
+        inorder(root)
+        
+        return min_diff
+
+
+    
